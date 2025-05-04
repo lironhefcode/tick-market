9e729c448362e6d40c1196f339f0984824aae6ae
lironhefcode <lironheftman@gmail.com>
2025-05-04 19:56:27 +0300

    logout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,18 @@
-import express from "express";
-const router = express.Router();
+import express, { Request, Response } from "express"
+import jwt from "jsonwebtoken"
+const router = express.Router()
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("hello world");
-});
-export { router as currentUserRouter };
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+  try {
+    debugger
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null })
+  }
+})
+
+export { router as currentUserRouter }
