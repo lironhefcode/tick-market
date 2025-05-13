8d2ed5b8da166370c183a9dac02fb7e811d7f0d2
lironhefcode <lironheftman@gmail.com>
2025-05-13 16:44:46 +0300

    new npm cusotm packge

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -5,8 +5,8 @@ import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from "./routes/signin"
 import { signoutRouter } from "./routes/signout"
 import { signupRouter } from "./routes/signup"
-import { errorHandler } from "./middlewares/error-handler"
-import { NotFoundError } from "./errors/notfound-error"
+
+import { errorHandler, NotFoundError } from "@lironticket/common"
 import cookieSession from "cookie-session"
 import e from "express"
 const app = express()
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
+import { currentUser } from "@lironticket/common"
 import express, { Request, Response } from "express"
-import { currentUser } from "../middlewares/current-user"
-import { requireAuth } from "../middlewares/require-auth"
+
 const router = express.Router()
 
 router.get(
